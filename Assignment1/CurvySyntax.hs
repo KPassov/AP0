dab4e882515eb64c9deb3752b1644bcfad18e853
kpassov <kasper.passov@gmail.com>
2013-09-10 14:13:07 +0200

    Well, it's a start

--- Assignment1/CurvySyntax.hs
+++ Assignment1/CurvySyntax.hs
@@ -1,8 +1,42 @@
 module CurvySyntax where
 
 import CurveAST
-import Curves
 
-{- parseString :: String -> Either Error Program -}
+data Parser a = Parser (String -> [(a,String)])
 
-{- parseFile :: FilePath -> IO (Either Error Program)parseFile :: FilePath -> IO (Either Error Program) -}
+data Error = String | Nothing 
+
+item :: Parser Char
+item =  Parser (\cs -> case cs of
+                       ""     -> []
+                       (c:cs') -> [(c,cs')])
+
+parse :: Parser a -> String -> [(a, String)]
+parse (Parser p) = p
+
+instance Monad Parser where
+    return a = Parser (\cs -> [(a,cs)])
+    p >>= f  = Parser (\cs -> concat  [parse (f a) cs' |
+                                      (a,cs') <- parse p cs])
+class Monad m => MonadZero m where
+      zero :: m a
+ 
+class MonadZero m => MonadPlus m where
+      (++) :: m a -> m a -> m a
+
+instance MonadZero Parser where
+      zero = Parser (\cs -> [])
+
+instance MonadPlus Parser where
+      p ++ q = Parser (\cs -> parse p cs ++ parse q cs)   
+
+
+
+undef :: t
+undef = undef
+
+parseString :: String -> Either Error Program
+parseString _ = undef
+
+parseFile :: FilePath -> IO (Either Error Program)
+parseFile _ = undef

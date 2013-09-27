cf7c8f060b0e575f20df15b30a503ad6cb1a0c34
Simon <smaibom@gmail.com>
2013-09-27 11:03:07 +0200

    part 1 done, notprime bugged

--- Assignment3/nats.pl
+++ Assignment3/nats.pl
@@ -28,7 +28,11 @@ add(s(X),Y,s(Z)) :- add(X, Y, Z).
 minus(X,Y,Z) :- add(Y,Z,X).
 
 % mult(X,Y,Z) => Z = X * Y
-% mult(X,Y,Z) :- 
+mult(z,Y,z).
+mult(s(X),Y,Z) :-
+    less(P,Z)
+    mult(X,Y,P),
+    add(P,Y,Z).
 
 % mod(X,Y,Z) => Z = X mod Y
 mod(Z,Y,Z) :- less(Z,Y).
@@ -40,16 +44,29 @@ mod(X,Y,Z) :- less(z,Y), minus(X,Y,H), mod(H,Y,Z).
 
 % notprime(X) => X is not a prime
 notprime(s(z)).
-notprime(X) :- less(s(z),Y), less(Y,X), mod(X,Y,z).
+notprime(s(X)) :- prime(s(X)), notprime(X).
+notprime(s(X)) :- less(s(z),Y), less(Y,X), mod(X,Y,z), n2i(X,P), write(P).
 
 % prime(X) => X is a prime
-%prime(
+prime(X) :- notdividable(X,X), n2i(X,P), write(P).
+
+%notdividable(X,Y) => X / Y = False 
+notdividable(X,s(s(z))).
+notdividable(X,s(Y)) :- less(Z,X), less(z,Z), mod(X,Y,Z), notdividable(X,Y).
 
 % listPrimes(L,X) => L is the list of primes small than or equal to X
-%listPrimes( ... ) :- ...
+listPrimes([P], T, z).
+listPrimes([P], T, X) :-
+    prime(X),
+    minus(X,z,S),
+    listPrimes([X|P],T,S).
+    
+    
+    
+%listPrimes([],X).
+
 
 % superprime(X) => X is a super-prime
-%superprime( ... ) :- ...
 
 
 %%%---------------------------------------------------------------------
